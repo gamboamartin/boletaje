fd359703f8e3e0baad7777969a777de2a0a4fdda
mgamboa <gamboa.martin@gmail.com>
2022-10-05 15:05:33 -0500

    v0.10.0 Se integra alta

--- views/bol_invitacion/alta.php
+++ views/bol_invitacion/alta.php
@@ -5,37 +5,24 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
-                    <form method="post" action="<?php echo $controlador->link_nom_nomina_alta_bd; ?>" class="form-additional">
+                    <form method="post" action="<?php echo $controlador->link_bol_invitacion_alta_bd; ?>" class="form-additional">
 
                         <?php include (new views())->ruta_templates."head/title.php"; ?>
                         <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
                         <?php include (new views())->ruta_templates."mensajes.php"; ?>
 
+                        <?php echo $controlador->inputs->evento; ?>
                         <?php echo $controlador->inputs->nombre; ?>
-                        <?php echo $controlador->inputs->select->nom_periodo_id; ?>
-                        <?php echo $controlador->inputs->select->em_empleado_id; ?>
-                        <?php echo $controlador->inputs->select->org_puesto_id; ?>
-                        <?php echo $controlador->inputs->select->cat_sat_tipo_contrato_nom_id; ?>
-                        <?php echo $controlador->inputs->select->nom_conf_empleado_id; ?>
-                        <?php echo $controlador->inputs->select->em_cuenta_bancaria_id; ?>
-                        <?php echo $controlador->inputs->rfc; ?>
-                        <?php echo $controlador->inputs->curp; ?>
-                        <?php echo $controlador->inputs->nss; ?>
-                        <?php echo $controlador->inputs->folio; ?>
-                        <?php echo $controlador->inputs->fecha; ?>
-                        <?php echo $controlador->inputs->fecha_inicio_rel_laboral; ?>
-                        <?php echo $controlador->inputs->select->cat_sat_tipo_nomina_id; ?>
-                        <?php echo $controlador->inputs->select->cat_sat_periodicidad_pago_nom_id; ?>
-                        <?php echo $controlador->inputs->fecha_pago; ?>
-                        <?php echo $controlador->inputs->fecha_inicial_pago; ?>
-                        <?php echo $controlador->inputs->fecha_final_pago; ?>
-                        <?php echo $controlador->inputs->num_dias_pagados; ?>
-                        <?php echo $controlador->inputs->salario_diario; ?>
-                        <?php echo $controlador->inputs->salario_diario_integrado; ?>
-                        <?php echo $controlador->inputs->subtotal; ?>
-                        <?php echo $controlador->inputs->descuento; ?>
-                        <?php echo $controlador->inputs->total; ?>
-
+                        <?php echo $controlador->inputs->ap; ?>
+                        <?php echo $controlador->inputs->am; ?>
+                        <?php echo $controlador->inputs->nombre_completo; ?>
+                        <?php echo $controlador->inputs->licenciatura; ?>
+                        <?php echo $controlador->inputs->generacion; ?>
+                        <?php echo $controlador->inputs->domicilio; ?>
+                        <?php echo $controlador->inputs->fecha_hora; ?>
+                        <?php echo $controlador->inputs->plantel; ?>
+                        <?php echo $controlador->inputs->n_boletos; ?>
+                        <?php echo $controlador->inputs->n_boletos_extra; ?>
                         <?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
 
                     </form>
