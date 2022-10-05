c91e06959408b2cf4db5561bd989dea827d07054
mgamboa <gamboa.martin@gmail.com>
2022-10-05 11:46:20 -0500

    v0.4.0 Se integra invitacion en operacion

--- orm/bol_invitacion.php
+++ orm/bol_invitacion.php
@@ -3,9 +3,9 @@ namespace gamboamartin\boletaje\models;
 use base\orm\modelo;
 use PDO;
 
-class bol_invitado extends modelo{
+class bol_invitacion extends modelo{
     public function __construct(PDO $link){
-        $tabla = 'bol_invitado';
+        $tabla = 'bol_invitacion';
         $columnas = array($tabla=>false);
         $campos_obligatorios = array();
 
--- templates/directivas/bol_invitacion_html.php
+++ templates/directivas/bol_invitacion_html.php
@@ -5,7 +5,7 @@ namespace html;
 use gamboamartin\system\html_controler;
 
 
-class bol_invitado_html extends html_controler {
+class bol_invitacion_html extends html_controler {
 
 
 
