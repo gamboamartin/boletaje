5db3a9593301b71257f6c161535b4ceb5a269c29
mgamboa <gamboa.martin@gmail.com>
2022-10-05 12:28:30 -0500

    v0.5.0 Se integra requirted en entidad

--- orm/bol_invitacion.php
+++ orm/bol_invitacion.php
@@ -7,7 +7,9 @@ class bol_invitacion extends modelo{
     public function __construct(PDO $link){
         $tabla = 'bol_invitacion';
         $columnas = array($tabla=>false);
-        $campos_obligatorios = array();
+        $campos_obligatorios = array('descripcion','codigo','codigo_bis','descripcion_select','n_boletos',
+            'n_boletos_extra','n_ingresos','evento','nombre_completo','licenciatura','generacion','domicilio',
+            'fecha_hora','plantel');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas);
