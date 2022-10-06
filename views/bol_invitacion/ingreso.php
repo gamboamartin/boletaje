a776bdf954ecb394081b9cf925e30dc07eef9f41
[user] <angel.martinez@ivitec.com.mx>
2022-10-06 14:52:41 -0500

    v0.14.0 Se inicializa generador de Codigo QR

--- views/bol_invitacion/ingreso.php
+++ views/bol_invitacion/ingreso.php
@@ -1,9 +1,36 @@
+<?php
+include('vendor/autoload.php');
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+
+if(isset($_POST["submit"]) && !empty($_POST["submit"])) {
+    $nombre = $_POST["nombre"];
+    $qr = QrCode::create($nombre);
+    $writer = new PngWriter();
+    $writer->write($qr)->saveToFile("QR.PNG");
+}
+?>
+
 <html>
 <head>
     <title>Html-Qrcode Demo</title>
 <body>
+
+<div class="widget-box box-container form-main widget-form-cart">
+<H1>Lector de Codigo QR</H1>
 <div id="qr-reader" style="width:500px"></div>
 <div id="qr-reader-results"></div>
+</div>
+
+
+<div class="widget-box box-container form-main widget-form-cart">
+    <form method="POST">
+        <H1>Generador de Codigo QR</H1>
+        <input type="text" name="nombre">
+        <br><br>
+        <input type="submit" name="submit" value="Enviar Formulario">
+    </form>
+</div>
 </body>
 
 <!--<script src="/js/html5-qrcode.min.js"></script>-->
