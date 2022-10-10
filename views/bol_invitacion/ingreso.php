0f681c4f7df8bd7550ae619bdfff840771a68973
[user] <angel.martinez@ivitec.com.mx>
2022-10-10 13:45:11 -0500

    v0.32.0 Se agregan estilos a generador qr

--- views/bol_invitacion/ingreso.php
+++ views/bol_invitacion/ingreso.php
@@ -1,9 +1,20 @@
 <?php /** @var gamboamartin\boletaje\controllers\controlador_bol_invitacion $controlador */; ?>
 <?php use config\views; ?>
 
-<div class="widget-box box-container form-main widget-form-cart" style="text-align: center;">
+<style>
+    .text_align{
+        text-align: center;
+    }
+    .qr_div{
+        margin: 0 auto;
+        width:66vh;
+        border: 0px; !important;
+    }
+</style>
+
+<div class="widget-box box-container form-main widget-form-cart text_align">
     <H1>Lector de Codigo QR</H1>
-    <div id="qr-reader" style="width:66vh;margin: 0 auto;text-align: center;border: 0 !important;"></div>
+    <div id="qr-reader" class="qr_div text_align"></div>
     <div id="qr-reader-results"></div>
     <br>
     <div class="col-sm-2 "></div>
