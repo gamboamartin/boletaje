e90cd983c27647cda2a72ca468a0a63931cd4a56
mgamboa <gamboa.martin@gmail.com>
2022-10-13 13:27:05 -0500

    v0.44.2 Se ajustan letras

--- controllers/controlador_bol_invitacion.php
+++ controllers/controlador_bol_invitacion.php
@@ -803,13 +803,25 @@ class controlador_bol_invitacion extends system {
 
         $pdf->Image(file: $file,x: 65,y: 60,w: 83);
 
-        $pdf->SetXY(0,188);
-        $pdf->Multicell(w: 215, h: 16, txt: utf8_decode('Plantel '.$bol_invitacion->bol_invitacion_plantel), align: 'C', fill: true);
+        $bol_invitacion_plantel = str_replace('  ', ' ', $bol_invitacion->bol_invitacion_plantel);
+        $bol_invitacion_plantel = str_replace('  ', ' ', $bol_invitacion_plantel);
+        $bol_invitacion_plantel = str_replace('  ', ' ', $bol_invitacion_plantel);
+        $bol_invitacion_plantel = str_replace('  ', ' ', $bol_invitacion_plantel);
+        $bol_invitacion_plantel = strtoupper($bol_invitacion_plantel);
 
+        $pdf->SetXY(0,188);
+        $pdf->Multicell(w: 215, h: 16, txt: utf8_decode('PLANTEL: '.$bol_invitacion_plantel), align: 'C', fill: true);
+
+
+        $bol_invitacion_licenciatura = str_replace('  ', ' ', $bol_invitacion->bol_invitacion_licenciatura);
+        $bol_invitacion_licenciatura = str_replace('  ', ' ', $bol_invitacion_licenciatura);
+        $bol_invitacion_licenciatura = str_replace('  ', ' ', $bol_invitacion_licenciatura);
+        $bol_invitacion_licenciatura = str_replace('  ', ' ', $bol_invitacion_licenciatura);
+        $bol_invitacion_licenciatura = strtoupper($bol_invitacion_licenciatura);
 
         $pdf->SetXY(0,216);
-        $pdf->SetFont(family: 'Times',size:  22);
-        $pdf->Multicell(w: 215, h: 10, txt: utf8_decode($bol_invitacion->bol_invitacion_licenciatura), align: 'C');
+        $pdf->SetFont(family: 'Times',size:  20);
+        $pdf->Multicell(w: 215, h: 10, txt: utf8_decode($bol_invitacion_licenciatura), align: 'C');
 
         $pdf->SetXY(0,235);
         $pdf->SetFont(family: 'Times', style: 'B',size:  23);
@@ -818,6 +830,7 @@ class controlador_bol_invitacion extends system {
         $bol_invitacion_nombre_completo = str_replace('  ', ' ', $bol_invitacion_nombre_completo);
         $bol_invitacion_nombre_completo = str_replace('  ', ' ', $bol_invitacion_nombre_completo);
         $bol_invitacion_nombre_completo = str_replace('  ', ' ', $bol_invitacion_nombre_completo);
+        $bol_invitacion_nombre_completo = strtoupper($bol_invitacion_nombre_completo);
 
         $pdf->Multicell(w: 215, h: 16, txt:utf8_decode($bol_invitacion_nombre_completo), align: 'C');
 
